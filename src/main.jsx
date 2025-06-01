60e165a1329da7579b36730b635f3dc746168834
AbhishekPati332 <abhishekpatidar332@gmail.com>
2025-06-01 09:16:15 +0530

    configures auto import sorting for eslint

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
+// Library imports
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+//component imports
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </StrictMode>,
 )
