ed6cbf1323822fdfe5f64c7c2741f433354fc9e6
AbhishekPati332 <abhishekpatidar332@gmail.com>
2025-06-04 08:07:33 +0530

    setup of axios instance done

---
+++ src/Helpers/axiosinstance.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:5173/api/v1";
+
+const axiosInstance = axios.create();
+
+axiosInstance.defaults.baseURL = BASE_URL;
+axiosInstance.defaults.withCredentials = True;
+
+export default axiosInsatnce;
+
+
